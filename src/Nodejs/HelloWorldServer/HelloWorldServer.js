a6289a84b913f7f559c6d931bbe608a227f244c0
Kelvin D. Meeks <kmeeks@intltechventures.com>
2018-11-19 11:22:35 -0800

    initial commit

---
+++ src/Nodejs/HelloWorldServer/HelloWorldServer.js
@@ -0,0 +1,13 @@
+/**
+// source: 
+// https://kubernetes.io/docs/tutorials/hello-minikube/ 
+**/ 
+var http = require('http');
+
+var handleRequest = function(request,response) {
+  console.log('Received request for URL: ' + request.url);
+  response.writeHead(200);
+  response.end('Hello World!');
+};
+var www=http.createServer(handleRequest);
+www.listen(8080);
