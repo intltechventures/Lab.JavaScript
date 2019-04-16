3cc3e534ca5eb4550b7c46b14b329d9c1e81ba71
Kelvin D. Meeks <kmeeks@intltechventures.com>
2019-04-15 22:08:22 -0700

    added ajv and ajv-cli

--- packages.sh
+++ packages.sh
@@ -1,3 +1,14 @@
 # Frequently used packages to install
+
+
 npm install -g typescript
+
+
 npm install -g eslint 
+
+
+# JSON Schema Validation
+# https://www.npmjs.com/package/ajv
+npm install -g ajv
+npm install -g ajv-cli
+
