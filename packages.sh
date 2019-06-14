d17f810266d06a2329945dc982a3220d4e5bd157
Kelvin D. Meeks <kmeeks@intltechventures.com>
2019-06-13 23:34:52 -0700

    added mssql and more links to tedious

--- packages.sh
+++ packages.sh
@@ -22,3 +22,19 @@ npm install -g grunt
 npm install -g axios
 # <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 
+
+# Tedious - The tedious module is a javascript implementation of the TDS protocol, which is supported by all modern versions of SQL Server
+npm install -g tedious 
+# https://tediousjs.github.io/tedious/
+# https://github.com/tediousjs/tedious
+# https://docs.microsoft.com/en-us/sql/connect/node-js/node-js-driver-for-sql-server?view=sql-server-2017
+# https://docs.microsoft.com/en-us/sql/connect/node-js/step-1-configure-development-environment-for-node-js-development?view=sql-server-2017
+# https://docs.microsoft.com/en-us/sql/connect/node-js/step-2-create-a-sql-database-for-node-js-development?view=sql-server-2017
+# https://docs.microsoft.com/en-us/sql/connect/node-js/step-3-proof-of-concept-connecting-to-sql-using-node-js?view=sql-server-2017
+
+
+# mssql 
+npm install -g mssql 
+# https://www.tutorialsteacher.com/nodejs/access-sql-server-in-nodejs
+# https://www.npmjs.com/package/mssql
+
